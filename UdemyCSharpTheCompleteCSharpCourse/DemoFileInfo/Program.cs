0ab2f4d38810bf322b29c2747b855993fd9a28c4
s1mpleDev <vuxuanhungmta@gmail.com>
2020-11-07 23:59:17 +0700

    hungvx-copy move file

--- UdemyCSharpTheCompleteCSharpCourse/DemoFileInfo/Program.cs
+++ UdemyCSharpTheCompleteCSharpCourse/DemoFileInfo/Program.cs
@@ -46,6 +46,32 @@ namespace DemoFileInfo
             streamWriter1.WriteLine("Hung");
             streamWriter1.Write("Không xuống dòng");
             streamWriter1.Close();
+
+            Console.WriteLine(fl4.FullName);
+            Console.WriteLine(fl4.Length);
+            Console.WriteLine(fl4.Name);
+            Console.WriteLine(fl4.DirectoryName);
+            Console.WriteLine(fl4.Directory);
+            Console.WriteLine(fl4.Extension);
+           
+            // move file, check required
+            // đk file2.txt ko đc exists, file1 phải có trước, move sẽ lấy theo name file đc khai báo theo path6
+            //string path5 = @"E:\folder1\file1.txt";
+            //string path6 = @"E:\folder2\file2.txt";
+            //FileInfo fl5 = new FileInfo(path5);
+            //FileInfo fl6 = new FileInfo(path6);
+            //fl5.MoveTo(path6);
+            //Console.WriteLine("{0} was moved to {1}.", path5, path6);
+
+
+            // copy file
+            // same như move nhưng file1 sẽ ko mất, mà có copy
+            string path5 = @"E:\folder1\file1.txt";
+            string path6 = @"E:\folder2\file2.txt";
+            FileInfo fl5 = new FileInfo(path5);
+            FileInfo fl6 = new FileInfo(path6);
+            fl5.CopyTo(path6);
+            Console.WriteLine("{0} was copyed to {1}.", path5, path6);
             Console.ReadKey();
         }
     }
