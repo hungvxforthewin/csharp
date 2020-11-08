a1bbdfdb1cdeeb5836621cdd12f4c82bcb7daf04
s1mpleDev <vuxuanhungmta@gmail.com>
2020-11-08 23:40:44 +0700

    hungvx-fileinfo rename file

--- UdemyCSharpTheCompleteCSharpCourse/DemoFileInfo/Program.cs
+++ UdemyCSharpTheCompleteCSharpCourse/DemoFileInfo/Program.cs
@@ -14,16 +14,16 @@ namespace DemoFileInfo
             string path = @"E:\MyFolder\test.txt";
             // file phải đã exists thì mới tạo để lấy thông tin file
             // create file 
-            FileInfo fl = new FileInfo(path);          
-            if (fl.Exists)
-            {                
-                Console.WriteLine("file created");
-            }
-            else
-            {
-                Console.WriteLine(fl.Name + " file not exists");
-                fl.Create(); // tương tự File
-            }
+            //FileInfo fl = new FileInfo(path);          
+            //if (fl.Exists)
+            //{                
+            //    Console.WriteLine("file created");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(fl.Name + " file not exists");
+            //    fl.Create(); // tương tự File
+            //}
             // create text
             //FileInfo fl2 = new FileInfo(path);
             //StreamWriter streamWriter = fl2.CreateText();
@@ -39,23 +39,25 @@ namespace DemoFileInfo
             //    Console.WriteLine(s);
             //}
             // append text --> same create text
-            FileInfo fl4 = new FileInfo(path);
-            StreamWriter streamWriter1 = fl4.AppendText();
-            streamWriter1.WriteLine("Vu");
-            streamWriter1.WriteLine("Thanh");
-            streamWriter1.WriteLine("Hung");
-            streamWriter1.Write("Không xuống dòng");
-            streamWriter1.Close();
+            //FileInfo fl4 = new FileInfo(path);
+            //StreamWriter streamWriter1 = fl4.AppendText();
+            //streamWriter1.WriteLine("Vu");
+            //streamWriter1.WriteLine("Thanh");
+            //streamWriter1.WriteLine("Hung");
+            //streamWriter1.Write("Không xuống dòng");
+            //streamWriter1.Close();
 
-            Console.WriteLine(fl4.FullName);
-            Console.WriteLine(fl4.Length);
-            Console.WriteLine(fl4.Name);
-            Console.WriteLine(fl4.DirectoryName);
-            Console.WriteLine(fl4.Directory);
-            Console.WriteLine(fl4.Extension);
-           
+            //Console.WriteLine(fl4.FullName);
+            //Console.WriteLine(fl4.Length);
+            //Console.WriteLine(fl4.Name);
+            //Console.WriteLine(fl4.DirectoryName);
+            //Console.WriteLine(fl4.Directory);
+            //Console.WriteLine(fl4.Extension);
+
             // move file, check required
             // đk file2.txt ko đc exists, file1 phải có trước, move sẽ lấy theo name file đc khai báo theo path6
+            // name file đc copy hay remove ko cần đk gì, đặt trùng với file1 cũng dc
+            // thao tác này gồm đổi tên file và di chuyển folder, kiểm tra folder mới phải exits
             //string path5 = @"E:\folder1\file1.txt";
             //string path6 = @"E:\folder2\file2.txt";
             //FileInfo fl5 = new FileInfo(path5);
@@ -66,12 +68,49 @@ namespace DemoFileInfo
 
             // copy file
             // same như move nhưng file1 sẽ ko mất, mà có copy
-            string path5 = @"E:\folder1\file1.txt";
-            string path6 = @"E:\folder2\file2.txt";
-            FileInfo fl5 = new FileInfo(path5);
-            FileInfo fl6 = new FileInfo(path6);
-            fl5.CopyTo(path6);
-            Console.WriteLine("{0} was copyed to {1}.", path5, path6);
+            // name file đc copy hay remove ko cần đk gì, đặt trùng với file1 cũng dc
+
+            //string path5 = @"E:\folder1\file1.txt";
+            //string path6 = @"E:\folder2\file1.txt";
+            //FileInfo fl5 = new FileInfo(path5);
+            //FileInfo fl6 = new FileInfo(path6);
+            //fl5.CopyTo(path6);
+            //Console.WriteLine("{0} was copyed to {1}.", path5, path6);
+
+            // change name file khi move
+            // required file1 phải có trước, và file2 đc đổi tên và chưa tồn tại
+            string path7 = @"E:\folder1\file1.txt";
+            FileInfo fl7 = new FileInfo(path7);
+            if (!fl7.Exists)
+            {
+                Console.WriteLine(fl7.FullName + " does not exits");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine(fl7.FullName + " exists");
+            string nameFile = Console.ReadLine();
+            if (nameFile == String.Empty)
+            {
+                Console.WriteLine("You not enter new file name. Cancel rename");
+                Console.ReadKey();
+                return;
+            }
+            FileInfo fl8 = new FileInfo(nameFile);
+            if (fl8.Exists)
+            {
+                Console.WriteLine("Can not rename file to " + fl8.FullName + ". File already exist.");
+                Console.ReadKey();
+                return;
+            }
+            // nếu ko có folder2 thì phải tạo, tạo folder cha, nếu tạo sẵn bằng tay rồi thì ko cần tạo nữa
+            // gọi là folder cha thì có thể chứa nhiều folder
+            fl8.Directory.Create();
+            fl7.MoveTo(fl8.FullName);
+            fl8.Refresh();
+            if (fl8.Exists)
+            {
+                Console.WriteLine("The file was renamed to " + fl8.FullName);
+            }
             Console.ReadKey();
         }
     }
