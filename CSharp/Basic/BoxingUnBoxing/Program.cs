e56b471ee9d7b733efa32434c49bded7f0a2443d
hungvx <vuxuanhungmta@gmail.com>
2022-08-09 15:05:27 +0700

    boxing, unboxing

---
+++ CSharp/Basic/BoxingUnBoxing/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BoxingUnBoxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Boxing");
+
+            int a = 10;
+            int b = a;
+            b = 100;
+
+            object c = a; // boxing
+            c = 111;
+
+            int j = (int)c; // unboxing
+
+            System.Console.WriteLine("a: " + a);
+            System.Console.WriteLine("b: " + b);
+            System.Console.WriteLine("c: " + c);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# csharp
+# csharp <ALL In One>
