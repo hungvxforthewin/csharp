b605eb3cda57a02c9c32e946ab6fa398a433b3e5
hungvx <vuxuanhungmta@gmail.com>
2022-08-09 16:48:34 +0700

    overloading

---
+++ CSharp/OOP/OverloadingOverride/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OverloadingOverride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Overloading");
+            int a;
+            a = Sum.tinhtong(1, 2);
+            System.Console.WriteLine("a: " + a);
+        }
+    }
+}
---
+++ CSharp/OOP/OverloadingOverride/Sum.cs
@@ -0,0 +1,16 @@
+namespace OverloadingOverride
+{
+    public class Sum
+    {
+        public static int tinhtong(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int tinhtong(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+    }
+}
