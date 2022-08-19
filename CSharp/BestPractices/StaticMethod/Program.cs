8943ab8978d580b30f3c10fccc0c9697a852f035
hungvx <vuxuanhungmta@gmail.com>
2022-08-19 08:50:50 +0700

    static method

---
+++ CSharp/BestPractices/StaticMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StaticMethod
+{
+    class Program
+    {
+        static int couter = 0;
+        string name = "Demo";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            name = "New Demo Program"; //Error: cannot access non-static members
+            SetRootFolder(@"C:\MyProgram"); //Error: cannot call non-static method
+
+            counter++; // can access static fields
+            Display("Hello World!"); // can call static methods
+        }
+        
+        // static method
+        static void displayName(string name)
+        {
+            System.Console.WriteLine(name);
+        }
+        public void setRootFolder(string path)
+        {
+
+        }
+    }
+}
