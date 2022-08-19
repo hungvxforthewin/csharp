c35da0617805c7a6fe4237a498af8e8f09616da5
hungvx <vuxuanhungmta@gmail.com>
2022-08-19 10:16:54 +0700

    static constructor

---
+++ CSharp/BestPractices/StaticConstructor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace StaticConstructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // static constructor luôn chạy đầu tiên, 1 lần khi static method, field đc call
+            // StopWatch.DisplayInfo();
+            // StopWatch.DisplayInfo();
+            //System.Console.WriteLine(StopWatch.aeg); 
+
+            // static construct được call 1 lần thì instance object đc create
+            //Hàm tạo tĩnh chỉ được gọi một lần bất cứ khi nào phương thức tĩnh được sử dụng hoặc tạo một thể hiện lần đầu tiên
+            StopWatch obj1 = new StopWatch();
+            StopWatch obj2 = new StopWatch();
+
+
+        }
+    }
+
+    public class StopWatch
+    {
+        // static constructor
+        static StopWatch()
+        {
+            Console.WriteLine("Static constructor called");
+            aeg = 10;
+        }
+
+        // instance constructor
+        public StopWatch()
+        {
+            Console.WriteLine("Instance constructor called");
+        }
+
+        // static method
+        public static void DisplayInfo()
+        {
+            Console.WriteLine("DisplayInfo called");
+        }
+
+        public static int aeg;
+
+        // instance method
+        public void Start() { }
+
+        // instance method
+        public void Stop() {  }
+    }
+}
--- CSharp/BestPractices/StaticMethod/Program.cs
+++ CSharp/BestPractices/StaticMethod/Program.cs
@@ -4,16 +4,18 @@ namespace StaticMethod
 {
     class Program
     {
-        static int couter = 0;
+        static int counter = 0;
         string name = "Demo";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            name = "New Demo Program"; //Error: cannot access non-static members
-            SetRootFolder(@"C:\MyProgram"); //Error: cannot call non-static method
+            //name = "New Demo Program"; //Error: cannot access non-static members
+            //SetRootFolder(@"C:\MyProgram"); //Error: cannot call non-static method
 
             counter++; // can access static fields
-            Display("Hello World!"); // can call static methods
+            displayName("Hello World!"); // can call static methods
+            
+            //public int ball = 1; // invalid syntax
         }
         
         // static method
