a827a9d475fee1a3625cff79cbf9458e7253cab0
hungvx <vuxuanhungmta@gmail.com>
2022-08-20 23:50:32 +0700

    partial class

---
+++ CSharp/Basic/PartialClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    // props
+    public partial class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+    }
+    
+    // method and constructor
+    public partial class Employee
+    {
+        //constructor
+        public Employee(int id, string name){
+            this.EmpId = id;
+            this.Name = name;
+        }
+
+        public void DisplayEmpInfo() {
+            Console.WriteLine(this.EmpId + " " + this.Name);
+        }
+    }
+}
