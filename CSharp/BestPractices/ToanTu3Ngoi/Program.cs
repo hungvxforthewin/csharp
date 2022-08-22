c966df416152e1725346a94b9a4c17978d435c74
hungvx <vuxuanhungmta@gmail.com>
2022-08-22 23:35:01 +0700

    toan tu 3 ngoi

---
+++ CSharp/BestPractices/ToanTu3Ngoi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ToanTu3Ngoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // đặt biến dưới dạng biểu thức
+            bool a = 3 < 2;
+            int b = a == true ? 10 : 1;
+            System.Console.WriteLine(a); // false
+            System.Console.WriteLine(b); // 1
+        }
+    }
+}
