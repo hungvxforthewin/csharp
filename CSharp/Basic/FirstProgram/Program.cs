19c89aea3c4356e49e2b1e814c94237c4b1e1392
hungvx <vuxuanhungmta@gmail.com>
2022-08-24 23:48:34 +0700

    first program c#

---
+++ CSharp/Basic/FirstProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // Reference of .NET Framework namespace
+
+namespace FirstProgram // namespace
+{
+    class Program // class name
+    {
+        // method--function
+        static void Main(string[] args)
+        {
+            string mess = "Hello World!"; // data type, variable, value of variable
+            Console.WriteLine(mess); // method to display value on console
+        }
+    }
+}
