cccd49a9589d402262bf445bc62ff5871f94f4f9
hungvx <vuxuanhungmta@gmail.com>
2022-08-25 23:52:31 +0700

    nullable reference type

---
+++ CSharp/BestPractices/NullableRefType/demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           testNullable();
+        }
+
+        public static void testNullable()
+        {
+                //int notNull = null;  // Compiler warns here
+            int? yesNull = null;
+            Nullable num = null;
+            //string text = null;
+            string text = "Hello world!";
+            PassNullReference(text); 
+        }
+        public static void PassNullReference (string str){
+            Console.WriteLine("The first char is " + str[0]);
+        }
+    }
+}
